76f1a2425501e2eaa9a652fd8516ef47a4332e14
Laweeza <louieshing@gmail.com>
2022-03-27 15:32:53 -0700

    created docker condfiguations and docker compose

--- server/index.js
+++ server/index.js
@@ -9,29 +9,6 @@ app.use(cors());
 app.use(express.json());
 // app.use(express.static('../client/'));
 
-app.get('/', async (req, res) => {
-  // await db.Patient.create({
-  //   name: 'Tendo',
-  // });
-  // await db.Question.bulkCreate([
-  //   {
-  //     content:
-  //       'Hi [Patient], on a scale of 1-10, would you recommend Dr. [Doctor] to a friend or family member? 1 = Would not recommend, 10 = Would strongly recommend',
-  //     question_type: 'rating',
-  //   },
-  //   {
-  //     content:
-  //       'Thank you. You were diagnosed with [Diagnosis]. Did Dr. [Doctor] explain how to manage this diagnosis in a way you could understand?',
-  //     question_type: 'boolean',
-  //   },
-  //   {
-  //     content:
-  //       'We appreciate the feedback, one last question: how do you feel about being diagnosed with [Diagnosis]?',
-  //     question_type: 'text',
-  //   },
-  // ]);
-});
-
 // Returns static JSON data
 app.get('/profile', (req, res) => {
   res.status(200).send(patientData);
@@ -80,6 +57,31 @@ app.get('/responses', async (req, res) => {
   }
 });
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, async () => {
   console.log(`app listening on port ${process.env.PORT}`);
+
+  const patient = await db.Patient.findAll();
+
+  if (!patient.length) {
+    await db.Patient.create({
+      name: 'Tendo',
+    });
+    await db.Question.bulkCreate([
+      {
+        content:
+          'Hi [Patient], on a scale of 1-10, would you recommend Dr. [Doctor] to a friend or family member? 1 = Would not recommend, 10 = Would strongly recommend',
+        question_type: 'rating',
+      },
+      {
+        content:
+          'Thank you. You were diagnosed with [Diagnosis]. Did Dr. [Doctor] explain how to manage this diagnosis in a way you could understand?',
+        question_type: 'boolean',
+      },
+      {
+        content:
+          'We appreciate the feedback, one last question: how do you feel about being diagnosed with [Diagnosis]?',
+        question_type: 'text',
+      },
+    ]);
+  }
 });
